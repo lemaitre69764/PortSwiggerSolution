be1f6da4490708df647af2f28416aafece71d700
madi <madimirzakup@gmail.com>
2024-12-27 21:20:36 +0500

    #

--- Server Side Topics/SQLi/Blind SQL injection with conditional responses/payload.py
+++ Server Side Topics/SQLi/Blind SQL injection with conditional responses/payload.py
@@ -1,2 +1,6 @@
-import requests
-import os
+def determine_response_length(inner_query, url, no_proxy=False):
+    log.info("Determining response length for query.")
+    sess = requests.Session()
+    for i in range(1, 5):
+        outer_query = format_length_query(inner_query, i)
+        
