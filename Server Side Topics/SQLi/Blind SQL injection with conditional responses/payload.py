379aad4e839ef953e92af8ed59b954a587395a6e
madi <madimirzakup@gmail.com>
2024-12-27 21:47:48 +0500

    #

--- Server Side Topics/SQLi/Blind SQL injection with conditional responses/payload.py
+++ Server Side Topics/SQLi/Blind SQL injection with conditional responses/payload.py
@@ -1,11 +1,24 @@
 import sys
 import logging
 import urllib3
+import concurrent.futures
 
 import requests
 
 import utils
 from shop import Shop
+
+log = logging.getLogger(__name__)
+logging.basicConfig(
+    stream=sys.stdout,
+    level=logging.INFO,
+    format="{asctime} [{threadName}][{levelname}] [{name}] {message}",
+    style = "{",
+    datefmt="%H:%M:%S",
+)
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
 
 def determine_response_length(inner_query, url, no_proxy=False):
     log.info("Determining response length for query.")
