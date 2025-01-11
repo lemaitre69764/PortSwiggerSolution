a1914c3236065b2ac79115eac1b74ea8e1cde73c
madi <madimirzakup@gmail.com>
2025-01-12 02:29:49 +0500

    #

--- Server Side Topics/SQLi/3rd_lab/shop.py
+++ Server Side Topics/SQLi/3rd_lab/shop.py
@@ -4,3 +4,24 @@ class Shop():
     def __init__(self, url):
         self.base_url = utils.normalize_url(url)
         self.login_url = self.base_url + "login"  
+        
+        
+
+def is_solved(url, no_proxy):
+    def _is_solved(url, no_proxy): # underline like a private to class 
+        log.info("Checking if solved.")
+        if no_proxy:
+            resp = requests.get(url)
+        else:
+            resp = requests.get(url, proxies=PROXIES, verify=False)
+        if "Congratulations, you solved the lab!" in resp.text:
+            log.info("Lab is solved!")
+            return True
+        
+    solved = _is_solved(url, no_proxy)
+    if solved:
+        return True
+    else:
+        time.sleep(2)
+        _is_solved(url, no_proxy)
+                
--- Server Side Topics/SQLi/3rd_lab/utils.py
+++ Server Side Topics/SQLi/3rd_lab/utils.py
@@ -26,22 +26,3 @@ def normalize_url(url):
         url = url + "/"
     return url
 
-
-def is_solved(url, no_proxy):
-    def _is_solved(url, no_proxy): # underline like a private to class 
-        log.info("Checking if solved.")
-        if no_proxy:
-            resp = requests.get(url)
-        else:
-            resp = requests.get(url, proxies=PROXIES, verify=False)
-        if "Congratulations, you solved the lab!" in resp.text:
-            log.info("Lab is solved!")
-            return True
-        
-    solved = _is_solved(url, no_proxy)
-    if solved:
-        return True
-    else:
-        time.sleep(2)
-        _is_solved(url, no_proxy)
-        
