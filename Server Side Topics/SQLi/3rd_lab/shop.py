ba685a2527f34dd4b2a9c6acc34b8c7c37f92c58
madi <madimirzakup@gmail.com>
2025-01-12 02:33:04 +0500

    #

--- Server Side Topics/SQLi/3rd_lab/shop.py
+++ Server Side Topics/SQLi/3rd_lab/shop.py
@@ -1,10 +1,21 @@
 import utils
 
 class Shop():
-    def __init__(self, url):
+    def __init__(self, url, no_proxy, session):
         self.base_url = utils.normalize_url(url)
-        self.login_url = self.base_url + "login"  
+        self.login_url = self.base_url + "login"
+        self.no_proxy = no_proxy
+        self.session = session
         
+    def login(self, username, password):
+        log.info("Geting login page.")
+        if self.no_proxy:
+            resp = self.session.get(self.login_url)
+        else:
+            resp = self.session.get(self.login_url, proxies=utils.PROXIES, verify=False)
+        if not resp.status_code == 200:
+            log.error("Could not get login page. Exiting script.")
+            sys.exit()
         
 
 def is_solved(url, no_proxy):
