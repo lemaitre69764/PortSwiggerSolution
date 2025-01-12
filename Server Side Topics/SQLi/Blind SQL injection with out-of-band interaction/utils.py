b730fbf90fcd7649f61fa1c1c29ceb657449cd9d
madi <madimirzakup@gmail.com>
2025-01-13 01:55:39 +0500

    #

--- Server Side Topics/SQLi/Blind SQL injection with out-of-band interaction/utils.py
+++ Server Side Topics/SQLi/Blind SQL injection with out-of-band interaction/utils.py
@@ -16,6 +16,17 @@ def parse_args(args: list):
     parser.add_argument("url", help="url of lab")
     return parser.parse_args() 
 
+
+def parse_args_collab(args: list):
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-n", "--no-proxy", default=False, action="store_true", help="do not use proxy"
+    )
+    parser.add_argument("url", help="url of lab")
+    parser.add_argument("collab", help="collaborator domain")
+    return parser.parse_args() 
+
+
 def normalize_url(url):
     if not url.endswith("/"):
         url = url + "/"
