f4396bede5441034042a9ab35c52aab3d2ae206e
madi <madimirzakup@gmail.com>
2025-01-13 03:09:53 +0500

    #

---
+++ Server Side Topics/SQLi/SQL injection UNION attack, retrieving data from other tables/utils.py
@@ -0,0 +1,23 @@
+import sys
+import argparse
+
+
+PROXIES = {
+    "http": "127.0.0.1:8080",
+    "https": "127.0.0.1:8080",
+}
+
+
+def parse_args(args: list):
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-n", "--no-proxy", default=False, action="store_true", help="do not use proxy"
+    )
+    parser.add_argument("url", help="url of lab")
+    return parser.parse_args() 
+
+def normalize_url(url):
+    if not url.endswith("/"):
+        url = url + "/"
+    return url
+
