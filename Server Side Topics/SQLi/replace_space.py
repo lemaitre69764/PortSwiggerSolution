1d105b3155284cd454b322de015046e4d0a6ab5e
madi <madimirzakup@gmail.com>
2025-01-13 18:06:50 +0500

    #

--- Server Side Topics/SQLi/replace_space.py
+++ Server Side Topics/SQLi/replace_space.py
@@ -1,7 +1,7 @@
 def replace_space(text):
     return text.replace(" ","%20")
 
-text = "Server Side Topics/SQLi/SQL injection UNION attack, finding a column containing text"
+text = "Server Side Topics/SQLi/SQL injection UNION attack, retrieving data from other tables/payload.py"
 new_text = replace_space(text)
 print(new_text)
 "gg"
