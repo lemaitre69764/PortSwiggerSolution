4f400f18c694b93156ead4337b42c36764b9745b
madi <madimirzakup@gmail.com>
2025-01-13 18:28:17 +0500

    #

--- Server Side Topics/SQLi/replace_space.py
+++ Server Side Topics/SQLi/replace_space.py
@@ -1,7 +1,7 @@
 def replace_space(text):
     return text.replace(" ","%20")
 
-text = "Server Side Topics/SQLi/Blind SQL injection with out-of-band interaction/payload.py"
+text = "fake\{flag\}"
 new_text = replace_space(text)
 print(new_text)
 "gg"
