80a6cbacd833f8a8845e02e15dd20398f3477f4d
madi <madimirzakup@gmail.com>
2025-01-14 21:04:31 +0500

    #

--- Server Side Topics/SQLi/w/payload.py
+++ Server Side Topics/SQLi/w/payload.py
@@ -1,23 +1,30 @@
 import sys
 import logging
 import argparse
-import urllib3 #25 line ->
+import urllib3
 
 import requests
 
 import utils
-log = logging.getLogger(__name__) #Ч : Создаёт логгер с именем текущего модуля (файла).
+from shop import Shop
+log = logging.getLogger(__name__) 
 logging.basicConfig(
-    stream=sys.stdout, #Логи выводятся в стандартный вывод (терминал).
+    stream=sys.stdout, 
     level=logging.INFO,
     format="{asctime} [{threadName}] [{levelname}][{name}] {message}",
     style="{",
     datefmt="%H:%M:%S",
 )
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning) #Removes warnings about insecure HTTPS connections
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning) 
 
 def main(args):
-    pass
+    shop = Shop(args.url, args.no_proxy)
+    num_columns = utils.determine_number_of_columns(shop.category_url, shop.no_proxy)
+    if not num_columns:
+        log.error("Couldn't determine number of columns. Exiting")
+        sys.exit(-1)
+    text_columns = utils.determine_text_columns(shop.category_url, shop.no_proxy, num_columns)
+    print(num_columns, text_columns)
 
 if __name__ == "__main__":
     args = utils.parse_args(sys.argv)
