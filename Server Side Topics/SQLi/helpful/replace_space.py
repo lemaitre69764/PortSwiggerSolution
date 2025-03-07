3a639fe5ce86e74ea54121c26dd4c650a9a31ea3
madi <madimirzakup@gmail.com>
2025-03-07 15:58:46 -0600

    Auto commit - 2025-03-07 15:58:46

--- Server Side Topics/SQLi/helpful/replace_space.py
+++ Server Side Topics/SQLi/helpful/replace_space.py
@@ -1,7 +1,7 @@
 def replace_space(text):
     return text.replace(" ","%20")
 
-text = "SQL injection with filter bypass via XML encoding"
+text = ""
 new_text = replace_space(text)
 print(new_text)
 "gg"
